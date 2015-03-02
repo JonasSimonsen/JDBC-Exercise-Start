06535882e86fdab506d825b845908d8efb7b890c
sofus <sofusalbertsen@gmail.com>
2015-03-02 13:27:33 +0100

    Initial project commit. Adapted the DB URL to match the new DB.

---
+++ JDBC Execise Start/src/DB.java
@@ -0,0 +1,7 @@
+public class DB {
+	public static String driver = "oracle.jdbc.driver.OracleDriver";
+	public static String URL = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
+	public static String ID = "";
+	public static String PW = "";
+}
+
---
+++ JDBC Execise Start/src/SelectApp.java
@@ -0,0 +1,53 @@
+
+import java.sql.*;
+
+public class SelectApp 
+{
+    public static void main(String[] args) throws SQLException
+    {
+        ResultSet rs = null;
+        Statement statement = null;
+        Connection connection = null;
+        
+        try 
+        {  
+          //=== Load the JDBC-driver
+          Class.forName(DB.driver);
+          
+          //=== Connect to the database
+          connection = DriverManager.getConnection(DB.URL, DB.ID,DB.PW );            
+              
+          //==== Instantiate a statement object 
+          statement = connection.createStatement();
+          
+          //=== Build an SQL-query-statement
+          String query = "SELECT * FROM parts";
+          
+          //=== Execute the query and receive the result
+          rs = statement.executeQuery(query);
+          System.out.println("== Parts ==");
+          //=== read the result
+          while (rs.next()) 
+          {
+            System.out.println(
+                rs.getInt(1)+"\t" + 
+                rs.getString(2)+"\t" + 
+                rs.getInt(3)+"\t" + 
+                rs.getDouble(4)+"\t" + 
+                rs.getInt(5));
+          }       
+        }
+        //=== If database driver is unavailable or query fails
+        catch (Exception ee) 
+        { 
+          System.out.println("fail");
+          System.err.println(ee); 
+        }        
+        //=== Always close the statement and connection
+        finally
+        {
+          statement.close();
+          connection.close(); 
+        }        
+    }
+}
